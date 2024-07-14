14823bd40f6614677f4c66005f756c49385856c0
KillerQueen <cuiao971@gmail.com>
2024-07-14 11:34:39 +0800

    Add files via upload

---
+++ match/src/main/java/com/jo/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.jo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jo.model.domain.User;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
